11bd9983eb6980e8f87f7081fbe9f8b66727453f
yurik763 <yurik763@tut.by>
2019-06-01 14:19:06 +0300

    Keep values on post

--- app.rb
+++ app.rb
@@ -29,16 +29,17 @@ end
 
 
 get '/visit' do
+  @c = Client.new
   erb :visit
 end
 
 post '/visit' do
 
-  c = Client.new params[:client]
-  if c.save
+  @c = Client.new params[:client]
+  if @c.save
     erb "<h2> Спасибо, вы записались ! </h2>"
     else
-      @error = c.errors.full_messages.first
+      @error = @c.errors.full_messages.first
       erb :visit
     end
 end
